4d744dfbc2fc9bd2d860a9054f922d9f1a2e7845
jontehulten <78376822+jontehulten@users.noreply.github.com>
2021-02-09 13:42:08 +0100

    Uppdate CardDeck & Shuffle

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 'use strict';
 
+var rowCounter = 1;
+var deckCounter = 0;
+var deck = [];
+
 window.addEventListener("load", function () {
-    
-    var rowCounter = 1;
+
     var cards = document.querySelectorAll("img");
+    fillDeck();
+    ShuffleDeck();
     
     for(let i = 0; i < cards.length; i++){
         cards[i].addEventListener("click", function(e){
@@ -16,30 +21,55 @@ window.addEventListener("load", function () {
 });
 
 function ShowCard(e){
-
     let card = e.target;
-    let cardColor = Math.floor( Math.random() * 4) + 1;
-    let cardValue = Math.floor( Math.random() * 13) + 1;
-    let searchPath = "cards/";
-
-    // Clubs = 1
-    // Diamonds = 2
-    // Hearts = 3
-    // Spades = 4
-    if(cardColor === 1){
-        searchPath += "c-";
-    }
-    if(cardColor === 2){
-        searchPath += "d-";
-    }
-    if(cardColor === 3){
-        searchPath += "h-";
-    }
-    if(cardColor === 4){
-        searchPath += "s-";
+    card.src = deck[deckCounter]; 
+    deckCounter++;
+    
+    if(deckCounter === 51){
+        deckCounter = 0;
     }
 
-    searchPath += cardValue + ".png" ;
-    card.src = searchPath;
+}
+function ShuffleDeck(){
+ 
+    /* suffle arrays with strings stackOverflow
+    //https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+    */
+    var currentIndex = deck.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = deck[currentIndex];
+    deck[currentIndex] = deck[randomIndex];
+    deck[randomIndex] = temporaryValue;
+  }
+}
+function fillDeck(){
+
+    for(let i=1; i<5; i++){
+    for(let j = 1; j < 14; j++){
+        let searchPath = "cards/";
+        if(i === 1){
+            searchPath += "c-";
+        }
+        if(i === 2){
+            searchPath += "d-";
+        }
+        if(i === 3){
+            searchPath += "h-";
+        }
+        if(i === 4){
+            searchPath += "s-";
+        }
+        searchPath += j + ".png";
+        deck.push(searchPath);
+        }
+    }
 }
 
