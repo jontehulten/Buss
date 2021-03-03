57e21e3fbdfcdf9a466d8abe0384a0f1fb8cffaa
johanjohanss <62746961+johanjohanss@users.noreply.github.com>
2021-03-03 22:11:52 +0100

    Animation v1
    Animation när korten kommer in + animation när korten vänds

--- app.js
+++ app.js
@@ -3,43 +3,59 @@
 var rowCounter = 1;
 var deckCounter = 0;
 var deck = [];
-var message = document.querySelector(".message")
+var message = document.querySelector(".message");
+
+//Animationsvariabler
+let delay = 100; //Hur lång tid det tar innan animationerna börjar (ms)
+let delayInterval = 100; //Hur lång delay mellan varje korts animation (ms)
+let xStart = 800; //Startposition i x-led (%)
+let yStart = 80; //Startposition i y-led (%)
 
 window.addEventListener("load", function () {
 
     var cards = document.querySelectorAll("img");
+
     fillDeck();
     ShuffleDeck();
     placeStarterCards();
-    
-    for(let i = 0; i < cards.length; i++){
-        cards[i].addEventListener("click", function(e){
-            if( e.target.nodeName === "IMG") {
-                CheckCard(e);  
+
+    cards.forEach(card => {
+        card.style.opacity = 0;
+        animateIn(card, delay, xStart, yStart);
+        delay += delayInterval;
+    });
+
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].addEventListener("click", function (e) {
+            if (e.target.nodeName === "IMG") {
+                CheckCard(e);
             }
         });
     }
 });
 
-function CheckCard(e){
+function CheckCard(e) {
 
     message.textContent = "";
     let card = e.target;
     let rows = document.querySelectorAll(".row");
-    if(rowCounter === 1){
+    if (rowCounter === 1) {
 
         let childrens = rows[3].children;
 
-        for(let i = 0; i < rows[3].children.length; i++){
-            
-            if(childrens[i] === e.target.parentNode){
+        for (let i = 0; i < rows[3].children.length; i++) {
+
+            if (childrens[i] === e.target.parentNode) {
                 rowCounter++;
-                
+
                 let hiddenValue = document.getElementById(e.target.parentNode.id);
-                card.src = hiddenValue.value;
+
+                //Funktion för att byta src på kortet med animation
+                revealCard(card, hiddenValue);
+
                 let faceCard = card.src.split(/[-,.]/);
 
-                if(faceCard[1] == 1 || faceCard[1] == 10 || faceCard[1] == 11 ||faceCard[1] == 12 ||faceCard[1] == 13){
+                if (faceCard[1] == 1 || faceCard[1] == 10 || faceCard[1] == 11 || faceCard[1] == 12 || faceCard[1] == 13) {
                     rowCounter = 1;
                     message.textContent = "drick 2";
                     turnAllCards();
@@ -48,22 +64,25 @@ function CheckCard(e){
                 placeNewCardFromDeck(hiddenValue);
             }
         }
-        
+
     }
-    if(rowCounter === 2){
+    if (rowCounter === 2) {
 
         let childrens = rows[2].children;
 
-        for(let i = 0; i < rows[2].children.length; i++){
-            
-            if(childrens[i] === e.target.parentNode){
+        for (let i = 0; i < rows[2].children.length; i++) {
+
+            if (childrens[i] === e.target.parentNode) {
                 rowCounter++;
-                
+
                 let hiddenValue = document.getElementById(e.target.parentNode.id);
-                card.src = hiddenValue.value;
+
+                //Funktion för att byta src på kortet med animation
+                revealCard(card, hiddenValue);
+
                 let faceCard = card.src.split(/[-,.]/);
 
-                if(faceCard[1] == 1 || faceCard[1] == 10 || faceCard[1] == 11 ||faceCard[1] == 12 ||faceCard[1] == 13){
+                if (faceCard[1] == 1 || faceCard[1] == 10 || faceCard[1] == 11 || faceCard[1] == 12 || faceCard[1] == 13) {
                     rowCounter = 1;
                     message.textContent = "drick 4";
                     turnAllCards();
@@ -72,22 +91,25 @@ function CheckCard(e){
                 placeNewCardFromDeck(hiddenValue);
             }
         }
-        
+
     }
-    if(rowCounter === 3){
+    if (rowCounter === 3) {
 
         let childrens = rows[1].children;
 
-        for(let i = 0; i < rows[1].children.length; i++){
-            
-            if(childrens[i] === e.target.parentNode){
-                rowCounter++; 
-                
+        for (let i = 0; i < rows[1].children.length; i++) {
+
+            if (childrens[i] === e.target.parentNode) {
+                rowCounter++;
+
                 let hiddenValue = document.getElementById(e.target.parentNode.id);
-                card.src = hiddenValue.value;
+
+                //Funktion för att byta src på kortet med animation
+                revealCard(card, hiddenValue);
+
                 let faceCard = card.src.split(/[-,.]/);
 
-                if(faceCard[1] == 1 || faceCard[1] == 10 || faceCard[1] == 11 ||faceCard[1] == 12 ||faceCard[1] == 13){
+                if (faceCard[1] == 1 || faceCard[1] == 10 || faceCard[1] == 11 || faceCard[1] == 12 || faceCard[1] == 13) {
                     rowCounter = 1;
                     message.textContent = "drick 6";
                     turnAllCards();
@@ -96,44 +118,47 @@ function CheckCard(e){
                 placeNewCardFromDeck(hiddenValue);
             }
         }
-        
+
     }
-    if(rowCounter === 4){
+    if (rowCounter === 4) {
 
         let childrens = rows[0].children;
 
-        for(let i = 0; i < rows[0].children.length; i++){
-            
-            if(childrens[i] === e.target.parentNode){
-                rowCounter = 1; 
-                
+        for (let i = 0; i < rows[0].children.length; i++) {
+
+            if (childrens[i] === e.target.parentNode) {
+                rowCounter = 1;
+
                 let hiddenValue = document.getElementById(e.target.parentNode.id);
-                card.src = hiddenValue.value;
+
+                //Funktion för att byta src på kortet med animation
+                revealCard(card, hiddenValue);
+
                 let faceCard = card.src.split(/[-,.]/);
 
-                if(faceCard[1] == 1 || faceCard[1] == 10 || faceCard[1] == 11 ||faceCard[1] == 12 ||faceCard[1] == 13){
+                if (faceCard[1] == 1 || faceCard[1] == 10 || faceCard[1] == 11 || faceCard[1] == 12 || faceCard[1] == 13) {
                     rowCounter = 1;
                     message.textContent = "drick 8";
                     turnAllCards();
 
                 }
-                else{
-                    message.textContent = "Du vann bussresan!"; 
+                else {
+                    message.textContent = "Du vann bussresan!";
                 }
                 placeNewCardFromDeck(hiddenValue);
             }
         }
-        
+
     }
-    
-    if(deckCounter === 51){
+
+    if (deckCounter === 51) {
         deckCounter = 0;
     }
 }
 
-function ShuffleDeck(){
- 
-    /* suffle arrays with strings stackOverflow
+function ShuffleDeck() {
+
+    /* shuffle arrays with strings stackOverflow
     //https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
     */
     var currentIndex = deck.length, temporaryValue, randomIndex;
@@ -141,36 +166,36 @@ function ShuffleDeck(){
     // While there remain elements to shuffle...
     while (0 !== currentIndex) {
 
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
 
-    // And swap it with the current element.
-    temporaryValue = deck[currentIndex];
-    deck[currentIndex] = deck[randomIndex];
-    deck[randomIndex] = temporaryValue;
-  }
+        // And swap it with the current element.
+        temporaryValue = deck[currentIndex];
+        deck[currentIndex] = deck[randomIndex];
+        deck[randomIndex] = temporaryValue;
+    }
 }
 
-function fillDeck(){
+function fillDeck() {
 
-    for(let i=1; i<5; i++){
-    for(let j = 1; j < 14; j++){
-        let searchPath = "cards/";
-        if(i === 1){
-            searchPath += "c-";
-        }
-        if(i === 2){
-            searchPath += "d-";
-        }
-        if(i === 3){
-            searchPath += "h-";
-        }
-        if(i === 4){
-            searchPath += "s-";
-        }
-        searchPath += j + ".png";
-        deck.push(searchPath);
+    for (let i = 1; i < 5; i++) {
+        for (let j = 1; j < 14; j++) {
+            let searchPath = "cards/";
+            if (i === 1) {
+                searchPath += "c-";
+            }
+            if (i === 2) {
+                searchPath += "d-";
+            }
+            if (i === 3) {
+                searchPath += "h-";
+            }
+            if (i === 4) {
+                searchPath += "s-";
+            }
+            searchPath += j + ".png";
+            deck.push(searchPath);
         }
     }
 }
@@ -179,10 +204,10 @@ function placeStarterCards() {
 
     //Med hidden value kommer det att bli möjligt att fuska?
 
-    for(let i=1; i<11; i++){
+    for (let i = 1; i < 11; i++) {
         let hiddenValue = document.getElementById("card" + i);
         hiddenValue.value = deck[deckCounter];
-        deckCounter ++;
+        deckCounter++;
         console.log(hiddenValue.value);
     }
 }
@@ -195,11 +220,92 @@ function placeNewCardFromDeck(card) {
 
 function turnAllCards() {
     let cards = document.querySelectorAll("img");
-    setTimeout(function(){
+    setTimeout(function () {
 
-        for(let i=0; i<10; i++){
-           cards[i].src = "cards/card-game-back.png";
+        for (let i = 0; i < 10; i++) {
+            cards[i].src = "cards/card-game-back.png";
         }
     }, 1000);
 }
 
+
+function revealCard(card, hiddenValue) {
+    spinCardHide(card); //Kort roterar bort
+    setTimeout(function () { //Vänta 150 sekunder (så lång tid det tar för kortet att rotera bort)
+        card.src = hiddenValue.value; //byt src
+        spinCardShow(card); // snurra tillbaka kortet
+    }, 150);
+}
+
+//Animationsfunktioner
+function animateIn(card, delay, xStart, yStart) {
+
+    let keyframes = [
+        {
+            transform:
+                "rotate(25deg) translateX(" + xStart + "%) translateY(" + yStart + "%)",
+            opacity: 0,
+        },
+        {
+            transform:
+                "rotate(0deg) translateX(0%) translateY(0%)",
+            opacity: 1,
+        },
+    ];
+
+    let options = {
+        duration: 1000,
+        iterations: 1,
+        fill: "forwards",
+        easing: "ease",
+        delay: delay
+    };
+
+    card.animate(keyframes, options)
+}
+
+function spinCardHide(card) {
+    let keyframes = [
+        {
+            transform:
+                "rotateY(0deg)",
+        },
+        {
+            transform:
+                "rotateY(90deg)",
+        },
+    ];
+
+    let options = {
+        duration: 150,
+        iterations: 1,
+        fill: "forwards",
+        easing: "ease",
+        delay: 0
+    };
+
+    card.animate(keyframes, options)
+}
+
+function spinCardShow(card) {
+    let keyframes = [
+        {
+            transform:
+                "rotateY(90deg)",
+        },
+        {
+            transform:
+                "rotateY(0deg)",
+        },
+    ];
+
+    let options = {
+        duration: 200,
+        iterations: 1,
+        fill: "forwards",
+        easing: "ease",
+        delay: 0
+    };
+
+    card.animate(keyframes, options)
+}
